18ad78cffb53c784511600633df4c0ae657a43ad
ruslan <ruslanzlygostev@gmail.com>
2020-02-09 15:28:19 +0300

    v1
    Вёрстка тестовой страницы

---
+++ app/index.entry.js
@@ -0,0 +1,8 @@
+import 'reset-css';
+import './exterrnal-libs/layout/index.pcss'
+import './index.entry.pcss';
+import "./exterrnal-libs/breakpoints/index.pcss";
+
+document.addEventListener('DOMContentLoaded', () => {
+
+});
